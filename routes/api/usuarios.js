002c178bfdcf4ef809122c692636f4af7658007b
David Perez <rpprec@gmail.com>
2020-06-09 17:22:45 +0200

    añadida funcionalidad registro

--- routes/api/usuarios.js
+++ routes/api/usuarios.js
@@ -1,4 +1,31 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const moment = require('moment');
+
+const Usuario = require ('../../controller/usuario.controller')
+
+
+// TODO: registrar usuario
+
+router.post('/registro', async (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, 10);
+    const result = await Usuario.create(req.body);
+    if (result['affectedRows'] === 1) {
+        res.json({ success: 'Registro Correcto'});
+    } else {
+        res.json({ error: 'Error en el registro'})
+    }
+
+    res.json(req.body)
+});
+
+// TODO: loguear usuario
+
+router.post('/login', (req, res) => {
+
+});
+
 
 // GET http://localhost:3000/api/usuarios
 // obtener todos los usuarios
